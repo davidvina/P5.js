3b193db472f23b1d003e1ecb83b402274e4986a2
davidvina <davidvina@gmail.com>
2017-10-11 23:15:13 +0200

    loops

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,32 @@ var reload = browserSync.reload;
 
 // variables paths
 var path = {
-	src: 'SKETCHS/'
+  src: 'SKETCHS/',
 };
 
 // function to init browser-sync server
 gulp.task('init-browser-sync', function () {
-	browserSync.init({
-		server: {
-			baseDir: path.src,
-			directory: true
-		},
-		port: 8080
-	//	reloadDelay: 200
-	//	reloadOnRestart: false
-	//	proxy: 'local.dev'
-	});
+  browserSync.init({
+    server: {
+      baseDir: path.src,
+      directory: true,
+    },
+    port: 8080,
+    //	reloadDelay: 200
+    //	reloadOnRestart: false
+    //	proxy: 'local.dev'
+  });
 });
 
 // function to watch
 
 gulp.task('default', ['init-browser-sync'], function () {
 
-	// watch function
-	// gulp.watch(path.src + '**/*.*').on('change', browserSync.reload);
+  // watch function
+  // gulp.watch(path.src + '**/*.*').on('change', browserSync.reload);
 
-	// using manual reload
-	gulp.watch(path.src + '**/*.*').on('change', reload);
+  // using manual reload
+  gulp.watch(path.src + '**/*.*').on('change', reload);
 });
 
 //

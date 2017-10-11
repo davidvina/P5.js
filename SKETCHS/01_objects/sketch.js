64731ae7140437daafcb92a51cef7d605c3e298d
davidvina <davidvina@gmail.com>
2017-10-11 22:36:49 +0200

    loops on 01_objects

--- SKETCHS/01_objects/sketch.js
+++ SKETCHS/01_objects/sketch.js
@@ -5,19 +5,32 @@ var bubbles = [];
 function setup () {
 	// setup canvas
 	createCanvas(600, 400);
+	//
+	frameRate(30);
 
-	//add element to array
-	bubbles.push(new Bubble(100,100))
+	// for loop
+	for (var i = 0; i < 80; i++) {
+		//add element to array
+		bubbles.push(new Bubble(random(0,width),random(0,height)));
+	}
 
-
+	// show text on console
+	//console.log(random(0,width));
 }
 
 function draw () {
 	// put drawing code here
 	background(255);
 
-	bubbles[0].display();
-	bubbles[0].move();
+	// for in loop
+	for (i in bubbles) {
+		bubbles[i].display();
+		bubbles[i].move();
+	}
+	// fill(255,0,0);
+	// textSize(32);
+	// text(frameRate(), 10, 30);
+
 }
 
 // object template
